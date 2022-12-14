1eb44b58010a3a57595034ab5fe034f51bfbf0ec
Lorena Gomez <lorena.natali18@gmail.com>
2022-12-14 07:33:04 -0300

    Script arreglado

--- components/checkComplete.js
+++ components/checkComplete.js
@@ -1,15 +1,19 @@
-const checkComplete = () => {
+const checkComplete = (id) => {
     const i = document.createElement('i');
     i.classList.add('far', 'fa-check-square', 'icon');
-    i.addEventListener('click', completeTask);
+    i.addEventListener('click', (event) => completeTask(event, id));
     return i;
-};
- // Expresión de función inmediatamente invocada IIFE
-const completeTask = (event) => {
+  };
+  // Immediately invoked function expression IIFE
+  const completeTask = (event, id) => {
     const element = event.target;
     element.classList.toggle('fas');
     element.classList.toggle('completeIcon');
     element.classList.toggle('far');
-};
-
-export default checkComplete;
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    const index = tasks.findIndex((item) => item.id === id);
+    tasks[index]['complete'] = !tasks[index]['complete'];
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  };
+  
+  export default checkComplete;
--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 import { addTask } from './components/addTask.js';
-import { readTasks } from './components/readTasks.js';
+import { displayTasks } from './components/readTasks.js';
 
 const btn = document.querySelector('[data-form-btn]');
 
 //Arrow functions o funciones anonimas
 btn.addEventListener('click', addTask);
 
-readTasks();
+displayTasks();
 
-//localStorage.setItem('tasks', JSON.stringify (datos))
-//La manera correcta de transformar los datos en string es a través de JSON.stringify y aprovechamos para utilizar el setItem para almacenar los datos localmente.
+/*
+localStorage.setItem('tasks', JSON.stringify (datos))
+La manera correcta de transformar los datos en string es a través de JSON.stringify y aprovechamos para utilizar el setItem para almacenar los datos localmente.
 
-//tasksList.forEach((task) => {
-    //list.appendChild(createTask(task))
-//})
-//LocalStorage se comporta como un arreglo, podemos utilizar métodos de arreglo como el forEachpara iterar sobre los ítems. Después, cada ítem es pasado para el componente Tarea, lo cual será agregado dentro del elemento padre representado por lista.
+tasksList.forEach((task) => {
+    list.appendChild(createTask(task))
+})
+LocalStorage se comporta como un arreglo, podemos utilizar métodos de arreglo como el forEachpara iterar sobre los ítems. Después, cada ítem es pasado para el componente Tarea, lo cual será agregado dentro del elemento padre representado por lista.
+*/
 
-/*dates.sort((a, b) => {
+/*
+dates.sort((a, b) => {
     const firstDate = moment(a, 'DD/MM/YYYY')
     const secondDate = moment(b, 'DD/MM/YYYY')
     return firstDate - secondDate
-})}*/
-//Utilizamos el método sort para ordenación.
+})}
+Utilizamos el método sort para ordenación.
 
-/*tasks[index]['complete'] = !tasks[index]['complete']
-El signo de exclamación ! es un operador de negación, luego, niega el estado que era verdade, transformándolo en falso.*/
+tasks[index]['complete'] = !tasks[index]['complete']
+El signo de exclamación ! es un operador de negación, luego, niega el estado que era verdade, transformándolo en falso.
+*/
